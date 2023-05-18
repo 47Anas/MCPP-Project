b77c15076eca091e7ec734516bb00c479d4abb27
Anas` Asender <potatoisgood76@gmail.com>
2023-05-18 10:14:29 +0300

    -) Fixed the shitty bug BY(just rename the project cause seems like microsoft has issues with my name(Anas))

---
+++ asp.net/Anas'sBookShelf/Anas'sBookShelf.WepApi/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Anas_sBookShelf.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anas_sBookShelf.WepApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }    
+    }
+}
+
+
+
+
